10219001ca907d1075325e9e58b3e7ec66fec4aa
Kelton Karboviak <kelton.karboviak@gmail.com>
2017-03-20 20:56:34 -0500

    Added UsersTableSeeder to prevent needing to register after a DB refresh

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -15,6 +15,7 @@ class DatabaseSeeder extends Seeder
         Model::unguard();
 
         $this->call(UserRolesTableSeeder::class);
+        $this->call(UsersTableSeeder::class);
 
         $this->call(FundingSourcesTableSeeder::class);
         $this->call(YearlyBudgetsTableSeeder::class);
