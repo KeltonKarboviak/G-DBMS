23db316b4598ab73ebc84a2f51c9c45ff6c6ff48
keltonkarboviak <kelton.karboviak@gmail.com>
2017-03-21 17:43:13 +0000

    Minor change in app.blade.php to improve readability

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -51,7 +51,6 @@
                     <li><a href="{{ url('/student') }}">Student Info</a></li>
                 </ul>
 
-
                 <!-- Right Side Of Navbar -->
                 <ul class="nav navbar-nav navbar-right">
                     <!-- Authentication Links -->
@@ -61,7 +60,7 @@
                     @else
                         <li class="dropdown">
                             <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                                {{ Auth::user()->first_name . " " . Auth::user()->last_name  }} <span class="caret"></span>
+                                {{ Auth::user()->first_name . " " . Auth::user()->last_name }} <span class="caret"></span>
                             </a>
 
                             <ul class="dropdown-menu" role="menu">
