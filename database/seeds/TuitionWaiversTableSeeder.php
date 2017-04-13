be91729ca3050f1602cb90a1f1a15f1ffb82045b
keltonkarboviak <kelton.karboviak@gmail.com>
2017-04-13 00:06:12 +0000

    Fixed some inconsistencies within the Tuition Waiver Seeder with the semesters

--- database/seeds/TuitionWaiversTableSeeder.php
+++ database/seeds/TuitionWaiversTableSeeder.php
@@ -17,44 +17,44 @@ class TuitionWaiversTableSeeder extends Seeder
         $waivers = [
             // Tuition Waivers for Fall 2015
             [
-                'id' => 1, 'semester_id' => 5, 'student_id' => '8881111',
-                'date_received' => '2016-08-01', 'amount_received' => 1500.00,
+                'id' => 1, 'semester_id' => 1, 'student_id' => '8881111',
+                'date_received' => '2015-08-01', 'amount_received' => 1500.00,
                 'credit_hours' => 6, 'funding_source_id' => 1, 'received' => true
             ],
             [
-                'id' => 2, 'semester_id' => 5, 'student_id' => '8882222',
-                'date_received' => '2016-08-01', 'amount_received' => 1500.00,
+                'id' => 2, 'semester_id' => 1, 'student_id' => '8882222',
+                'date_received' => '2015-08-01', 'amount_received' => 1500.00,
                 'credit_hours' => 6, 'funding_source_id' => 1, 'received' => true
             ],
             [
-                'id' => 3, 'semester_id' => 5, 'student_id' => '8883333',
-                'date_received' => '2016-08-01', 'amount_received' => 2000.00,
+                'id' => 3, 'semester_id' => 1, 'student_id' => '8883333',
+                'date_received' => '2015-08-01', 'amount_received' => 2000.00,
                 'credit_hours' => 9, 'funding_source_id' => 1, 'received' => true
             ],
             [
-                'id' => 4, 'semester_id' => 5, 'student_id' => '8884444',
-                'date_received' => '2016-08-01', 'amount_received' => 2000.00,
+                'id' => 4, 'semester_id' => 1, 'student_id' => '8884444',
+                'date_received' => '2015-08-01', 'amount_received' => 2000.00,
                 'credit_hours' => 9, 'funding_source_id' => 1, 'received' => true
             ],
 
             // Tuition Waivers for Spring 2016
             [
-                'id' => 5, 'semester_id' => 6, 'student_id' => '8881111',
+                'id' => 5, 'semester_id' => 2, 'student_id' => '8881111',
                 'date_received' => '2016-01-01', 'amount_received' => 1500.00,
                 'credit_hours' => 6, 'funding_source_id' => 1, 'received' => true
             ],
             [
-                'id' => 6, 'semester_id' => 6, 'student_id' => '8882222',
+                'id' => 6, 'semester_id' => 2, 'student_id' => '8882222',
                 'date_received' => '2016-01-01', 'amount_received' => 1500.00,
                 'credit_hours' => 6, 'funding_source_id' => 1, 'received' => true
             ],
             [
-                'id' => 7, 'semester_id' => 6, 'student_id' => '8883333',
+                'id' => 7, 'semester_id' => 2, 'student_id' => '8883333',
                 'date_received' => null, 'amount_received' => 2000.00,
                 'credit_hours' => 9, 'funding_source_id' => 1, 'received' => false
             ],
             [
-                'id' => 8, 'semester_id' => 6, 'student_id' => '8884444',
+                'id' => 8, 'semester_id' => 2, 'student_id' => '8884444',
                 'date_received' => '2016-01-01', 'amount_received' => 2000.00,
                 'credit_hours' => 9, 'funding_source_id' => 1, 'received' => true
             ],
